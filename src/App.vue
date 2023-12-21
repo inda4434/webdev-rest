277f2b467ed54e8f9d759b2101a583c4f36b77b6
Chandler Carlin <chandlercarlin01@gmail.com>
2023-12-21 15:07:51 -0600

    fixed new incident

--- src/App.vue
+++ src/App.vue
@@ -258,7 +258,7 @@ function deleteCrimeMarker(markerId) {
 }
 
 const handleCrimeSelection = (crime) => {
-    const address = crime.block.split(" ").map((word, index) => index === 0 ? word.replace(/X/g, '0'): word).join(" ") + " Saint Paul, Minnesota";
+    const address = crime.block.split(" ").map((word, index) => index === 0 ? word.replace(/X/g, '0'): word).join(" ");
     console.log(address);
 
     getCoordinatesForAddress(address)
@@ -338,8 +338,7 @@ const getCoordinatesForAddress = (address) => {
 };
 
 function deleteCrime(caseNumber) {
-    const apiUrl = 'http://localhost:8000/remove-incident';
-    fetch(apiUrl, {
+    fetch(`${crime_url.value}/remove-incident?$`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json',
@@ -506,17 +505,18 @@ const newIncident = ref({
 });
 
 function newIncidentFunc(){
+    console.log(newIncident.value);
     fetch(`${crime_url.value}/new-incident`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(newIncidentFunc.value),
+        body: JSON.stringify(newIncident.value),
     })
     .then (response => {
         if (response.ok){
             console.log('new form added');
-            newIncidentFunc.value = {
+            newIncident.value = {
                 case_number: '',
                 date: '',
                 time: '',
@@ -528,6 +528,7 @@ function newIncidentFunc(){
             };
         }
         else {
+            console.log(response);
             console.log('sucks');
         }
     })
@@ -693,7 +694,7 @@ function newIncidentFunc(){
         <div class="grid-x grid-padding-x">
             <h5 class="cell small-12 large-12" style="font-weight: bold; font-size: medium; margin-top: 1rem;">Submit New Incident</h5>
             <div class="cell small-12 large-6">
-                <form id="new-incident" action="/new-incident" method="PUT">
+                <form id="new-incident" action="/new-incident" method="PUT" @submit.prevent="newIncidentFunc">
                     <!--case_number-->
                     <div class="cell small-12 large-6">
                         <label for="case_number">Case Number: </label>
@@ -736,7 +737,7 @@ function newIncidentFunc(){
                     </div>
                     <!--submit-->
                     <div class="cell small-12 large-6">
-                        <button id="sub_new_inc" type="submit" @click="newIncidentFunc">Submit</button>
+                        <button id="sub_new_inc" type="submit">Submit</button>
                     </div>
                 </form>
             </div>
